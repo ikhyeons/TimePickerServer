511122737077ed9bc055f6061e573e70d580900e
ikhyeons <skantrkwl789@naver.com>
2024-07-21 00:25:36 +0900

    Init

---
+++ src/main/java/com/ikhyeons/tp/time_picker_server/TimePickerServerApplication.java
@@ -0,0 +1,13 @@
+package com.ikhyeons.tp.time_picker_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimePickerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimePickerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ikhyeons/tp/time_picker_server/TimePickerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikhyeons.tp.time_picker_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimePickerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
